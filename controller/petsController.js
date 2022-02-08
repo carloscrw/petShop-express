dd8a18b22d6b9da27e73c00af5ae9a4856bc71c4
Carlos Henrique <carlos.c.r.w@gmail.com>
2022-02-08 20:21:44 -0300

    Controller e Rota 2

--- controller/petsController.js
+++ controller/petsController.js
@@ -2,7 +2,7 @@ const modelPets = require("../model/pets.json")
 
 const pestsController = {
     listaPets: (req, res) => {
-        res.send("modelPets")
+        res.send(modelPets)
     }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 let pestsController = require("../controller/petsController");
 const sevicosController = require('../controller/servicosController');
-let servicosController = require('../controller/servicosController')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
