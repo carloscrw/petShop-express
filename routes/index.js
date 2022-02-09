a72522e81e6837279f1ec2067b38be57ec26e684
Carlos Henrique <carlos.c.r.w@gmail.com>
2022-02-08 21:30:22 -0300

    views arquivos paginas ejs

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var router = express.Router();
 let pestsController = require("../controller/petsController");
-const sevicosController = require('../controller/servicosController');
+let sevicosController = require('../controller/servicosController');
+let usuarioController = require('../controller/usuarioController')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
-router.get('/pets', pestsController.listaPets)
-router.get('/servicos', sevicosController.listaServicos)
+router.get('/cadastro', usuarioController.cadastro);
+
+router.get('/pets', pestsController.listaPets);
+router.get('/servicos', sevicosController.listaServicos);
 
 module.exports = router;
